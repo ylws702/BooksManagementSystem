19245aae45f2ff051cf7ee7cb4a665ee92d53683
吴迪 <1443307319@qq.com>
2018-07-07 17:28:33 +0800

    Add headers.

--- BooksManagementSystem/ShowHelper.h
+++ BooksManagementSystem/ShowHelper.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdio>
 #include "User.h"
 #include "Book.h"
 class ShowHelper
--- BooksManagementSystem/UserMap.h
+++ BooksManagementSystem/UserMap.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <map>
 #include <fstream>
-#include <cstdlib>
+#include <cstring>
 #include "User.h"
 class UserMap
 {
